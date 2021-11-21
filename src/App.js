2bb4ec91aab8cba6c632602e759d2f92ee0bc3bf
YairAn <57504705+YairAn@users.noreply.github.com>
2021-11-21 16:20:55 +0200

    sign in form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet'
 import './App.css';
 import Clarifai from 'clarifai';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
@@ -6,6 +7,8 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo/Logo';
 import  Navigation  from './components/Navigation/Navigation.js';
 import Rank from './components/Rank/Rank';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
 
 
 /*key:  905373a88e3c423694d816565b91dcdf */
@@ -15,9 +18,30 @@ class App extends React.Component {
      this.state = {
        input: '',
        imageUrl: '',
+       box: {},
+       route: 'signin',
+       isSignedIn: false,
+
      }
    }
 
+   calculateFaceLocation = (data) => {
+    const clarifaiFace = data;
+    console.log(data);
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
 
    onInputChange = (event) => {
     this.setState({input: event.target.value}); 
@@ -48,20 +72,46 @@ class App extends React.Component {
       },
       body: raw
     };
+    let box_data = {};
     fetch("https://api.clarifai.com/v2/models/a403429f2ddf4b49b307e318f00e528b/versions/34ce21a40cc24b6b96ffee54aabff139/outputs", requestOptions)
       .then(response => response.text())
-      .then(result => console.log(JSON.parse(result, null, 2).outputs[0].data))
+      .then(result => {
+        const box_data = JSON.parse(result, null, 2).outputs[0].data.regions[0].region_info.bounding_box;
+        this.displayFaceBox(this.calculateFaceLocation(box_data))
+      })
       .catch(error => console.log('error', error));
+      
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
   }
 
   render(){
   return (
     <div className="App">
-     <Navigation />
-     <Logo />
-     <Rank />
-     <ImageLinkForm  onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-     <FaceRecognition imageUrl={this.state.input}/>
+      <Helmet>
+        <title>THE BRAIN</title>
+      </Helmet>
+     <Navigation onRouteChange={this.onRouteChange} isSignedIn={this.state.isSignedIn}/>
+     { this.state.route === 'home'
+     ?<div>
+       <Logo />
+       <Rank />
+       <ImageLinkForm  onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+       <FaceRecognition box={this.state.box} imageUrl={this.state.input}/>
+     </div>
+     : (
+      this.state.route === 'signin'
+      ? <SignIn onRouteChange={this.onRouteChange}/>
+      : <Register onRouteChange={this.onRouteChange}/>
+     )
+    }
     </div>
   );}
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,20 @@
 import React from "react";
 
-const Navigation = () => {
-    return(
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+    if(isSignedIn){
+        return(
+            <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+                <p onClick={ () => onRouteChange('signout') } className='ma3 b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib'>Sign Out</p>
+            </nav>
+        );
+    } else {
+        return(
         <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-            <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+            <p onClick={ () => onRouteChange('signin') } className='ma3 b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib'>Sign In</p>
+            <p onClick={ () => onRouteChange('register') } className='ma3 b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib'>Register</p>
         </nav>
-    );
+        );
+    }
 }
 
 export default Navigation;
